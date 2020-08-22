00ce27d3e08880f5625b49aee19b5a714e50281c
Prakash Dale <prakashdale@gmail.com>
2020-08-22 20:42:25 +0530

    Basic project.

---
+++ src/availability.api/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace availability.api
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+            => await CreateWebHostBuilder(args)
+                .Build()
+                .RunAsync();
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+            => WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
